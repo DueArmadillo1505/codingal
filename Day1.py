91cb830b06648de707003d4269e913b0ecb42e8f
DueArmadillo1505 <deshawnnyduah@gmail.com>
2025-11-01 18:34:33 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,29 @@
+num=12
+if num >0:
+    print("positive number")
+
+#Activity-2
+actual_cost=100
+sale_amount=120
+if (actual_cost>sale_amount):
+    amount=actual_cost-sale_amount
+    print("loss amount" , amount)
+elif (actual_cost-sale_amount):
+    amount= sale_amount-actual_cost
+    print ("profit amount", amount)
+else:
+    print("no profit no loss")
+
+#Activity-3
+n=10
+if n>9:
+    print("number is grteater than 15")
+else:
+    print("number is less than 15")
+
+#Activity-4
+num=13
+if num%2==0:
+    print("number is even")
+else: 
+    print("number is odd")    
